431c5a7c1697c83eb1c4b6f4391bf279669929e7
SolitaryM <zzzxxx804113401@gmail.com>
2020-02-27 19:08:53 +0800

    Implement q_free and change q_new q->size

--- queue.c
+++ queue.c
@@ -17,15 +17,21 @@ queue_t *q_new()
         return NULL;
     q->head = NULL;
     q->tail = NULL;
-    q->size++;
+    q->size = 0;
     return q;
 }
 
 /* Free all storage used by queue */
 void q_free(queue_t *q)
 {
-    /* TODO: How about freeing the list elements and the strings? */
-    /* Free queue structure */
+    if (!q)
+        return;
+    while (q->head) {
+        list_ele_t *tmp = q->head;
+        q->head = q->head->next;
+        free(tmp->value);
+        free(tmp);
+    }
     free(q);
 }
 
